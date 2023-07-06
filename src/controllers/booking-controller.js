eead8a9439d2d579b1571a4ab1fa4b44b3854bde
Floyden <floydenmonteiro@gmail.com>
2023-07-06 19:35:14 +0530

    made proxy compliant

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,6 +2,8 @@ const { BookingService } = require('../services');
 const { successResponse, ErrorResponse } = require('../utils/common');
 const { StatusCodes } = require('http-status-codes');
 
+const inMemDb = {};
+
 async function createBooking(req, res) {
   try {
     const response = await BookingService.createBooking({
@@ -19,15 +21,26 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
   try {
+    const idempotencyKey = req.headers['x-idempotency-key'];
+    if (!idempotencyKey) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: 'idempotency key missing' });
+    }
+    if (inMemDb[idempotencyKey]) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: 'Cannot retry on a successful payment' });
+    }
     const response = await BookingService.makePayment({
       totalCost: req.body.totalCost,
       userId: req.body.userId,
       bookingId: req.body.bookingId,
     });
+    inMemDb[idempotencyKey] = idempotencyKey;
     successResponse.data = response;
     return res.status(StatusCodes.OK).json(successResponse);
   } catch (error) {
-   
     ErrorResponse.error = error;
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
   }
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.delete('/api/v1/blogs', (req, res) => {});
 
 app.use('/api', apiRoutes);
+// app.use('/bookingService/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started the sever on PORT : ${ServerConfig.PORT}`);
